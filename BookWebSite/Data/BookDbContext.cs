7e625089d6708ab5108ef4548efcb6272b3bd787
Ignacio-Martinez98 <martinezigna98>
2021-11-05 11:35:03 -0300

    Agregar archivos de proyecto.

---
+++ BookWebSite/Data/BookDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BookWebSite.Models;
+using System.Data.Entity;
+
+namespace BookWebSite.Data
+{
+    public class BookDbContext:DbContext
+    {
+        public BookDbContext() : base("KeyDB") { }
+        public DbSet<Book> Books { get; set; }
+    }
+}
